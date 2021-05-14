5c2e91e06c01cacef21bd633842585674300f7da
ffarall <ffarall@github.com>
2021-05-14 10:56:46 +0200

    pushing for NIco to try

---
+++ input_output_generator.py
@@ -0,0 +1,40 @@
+# %%
+from pycocotools.coco import COCO
+from dataset_utils.filter_cats import filtered_cats
+import skimage as ski
+import numpy as np
+from tqdm import tqdm
+
+# %%
+TRAIN_IMAGES_DIRECTORY = "train-v0.4/train/images"
+TRAIN_ANNOTATIONS_PATH = "train-v0.4/train/annotations.json"
+
+# Build coco object pointing to the annotations
+coco = COCO(TRAIN_ANNOTATIONS_PATH)
+
+# Filter 16 more frequent categories
+n_cats = 16
+(cat_ids, cat_names, img_ids) = filtered_cats(coco, n=n_cats)
+
+# Get image relative paths
+images = coco.loadImgs(img_ids)
+img_paths = [img["file_name"] for img in images]
+
+
+# %%
+SIZE_X = 426
+SIZE_Y = 426
+
+train_images = []
+
+for img_path in tqdm(img_paths):
+    img_path = TRAIN_IMAGES_DIRECTORY + "/" + img_path
+    img = ski.io.imread(img_path)
+    img = ski.transform.resize(img, (SIZE_X, SIZE_Y))
+    train_images.append(img)
+
+x_train = np.array(train_images)
+
+# %%
+INPUT_ARRAY_PATH = "train-v0.4/train/images/input_array.npy"
+np.save(INPUT_ARRAY_PATH, x_train)
