db7a5a2e99eeee4b534972e9b83ba46c0d465648
gonzalodavidov <gdavidov@itba.edu.ar>
2021-05-14 13:00:43 +0200

    cambios para el gede de nico

--- model/main2.py
+++ model/main2.py
@@ -17,8 +17,8 @@ SIZE = 426
 input_shape = (SIZE, SIZE, 3)
 n_classes = 16
 
-X_test = []
-Y_test = []
+x_test = []
+y_test = []
 
 images = os.listdir(test_image_directory)
 for i, image_name in enumerate(images):
@@ -26,7 +26,7 @@ for i, image_name in enumerate(images):
         image = cv2.imread(test_image_directory+image_name, 0)
         image = Image.fromarray(image)
         image = image.resize((SIZE, SIZE))
-        X_test.append(np.array(image))
+        x_test.append(np.array(image))
 
 masks = os.listdir(test_mask_directory)
 for i, mask_name in enumerate(masks):
@@ -34,30 +34,30 @@ for i, mask_name in enumerate(masks):
         mask = cv2.imread(test_mask_directory+mask_name, 0)
         mask = Image.fromarray(mask)
         mask = mask.resize((SIZE, SIZE))
-        Y_test.append(np.array(mask))
+        y_test.append(np.array(mask))
 
 
-X_test = np.expand_dims(X_test, axis=3)
-X_test = normalize(X_test, axis=1)
-Y_test = np.expand_dims(np.array(Y_test), axis=3) / 255.  #change the 255 if masks already normalized
+x_test = np.expand_dims(x_test, axis=3)
+x_test = normalize(x_test, axis=1)
+y_test = np.expand_dims(np.array(y_test), axis=3) / 255.  #change the 255 if masks already normalized
 
-test_masks_cat = to_categorical(Y_test, num_classes=n_classes)
-y_test_cat = test_masks_cat.reshape((Y_test.shape[0], Y_test.shape[1], Y_test.shape[2], n_classes))  #check this line
+test_masks_cat = to_categorical(y_test, num_classes=n_classes)
+y_test_cat = test_masks_cat.reshape((y_test.shape[0], y_test.shape[1], y_test.shape[2], n_classes))  #check this line
 
 #Load Model
 model = build_vgg19_unet(input_shape, n_classes)
 model.load_weights('food_recognition_50_epochs_test.hdf5')
 
 #Calculate Accuracy
-_, acc = model.evaluate(X_test, y_test_cat)
+_, acc = model.evaluate(x_test, y_test_cat)
 print("Accuracy is = ", (acc * 100.0), "%")
 
 #IoU
-y_pred = model.predict(X_test)
+y_pred = model.predict(x_test)
 y_pred_argmax = np.argmax(y_pred, axis=3)
 
 IOU_keras = MeanIoU(num_classes=n_classes)  
-IOU_keras.update_state(Y_test[:,:,:,0], y_pred_argmax)
+IOU_keras.update_state(y_test[:,:,:,0], y_pred_argmax)
 print("Mean IoU =", IOU_keras.result().numpy())
 
 
