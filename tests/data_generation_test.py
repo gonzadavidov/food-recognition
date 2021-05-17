bedb33ee2c3c2a0ff5b16a76d48fb57424e57232
nicotrozzo <rtrozzo@itba.edu.ar>
2021-05-17 15:58:26 +0200

    Added one channel for background

--- tests/data_generation_test.py
+++ tests/data_generation_test.py
@@ -3,6 +3,7 @@
 import os
 from pycocotools.coco import COCO
 import matplotlib.pyplot as plt
+import cv2
 
 from dataset_filtering.data_generation import DataGeneration
 from dataset_filtering.filter_cats import filtered_cats
@@ -17,8 +18,8 @@ SIZE_Y = 426
 # Build coco object pointing to the annotations
 coco = COCO(TRAIN_ANNOTATIONS_PATH)
 
-# Filter 16 more frequent categories
-n_cats = 16
+# Filter 2 more frequent categories
+n_cats = 2
 (cat_ids, cat_names, img_ids) = filtered_cats(coco, n=n_cats)
 
 # Get image relative paths
@@ -27,10 +28,20 @@ img_paths = [img["file_name"] for img in images]
 
 data_gen = DataGeneration(coco, SIZE_X, SIZE_Y, cat_ids)
 
-i = 0
-image_path = os.path.join(TRAIN_IMAGES_DIRECTORY, img_paths[0])
+i = 106
+while True:
+    annotation_ids = coco.getAnnIds(imgIds=img_ids[i], catIds=cat_ids)
+    if len(annotation_ids) > 1:
+        break
+    i += 1
+print(i)
+image_path = os.path.join(TRAIN_IMAGES_DIRECTORY, img_paths[i])
 x_train = data_gen.x_sample(image_path)
-y_train = data_gen.y_sample(img_ids[0])
-plt.imshow(x_train)
-plt.imshow(y_train[:, :, 1], alpha=0.5)
-
+y_train = data_gen.y_sample(img_ids[i])
+plt.figure()
+plt.imshow(cv2.cvtColor(x_train, cv2.COLOR_BGR2RGB)); plt.axis('off')
+annotation_ids = coco.getAnnIds(imgIds=img_ids[i], catIds=cat_ids)
+annotations = coco.loadAnns(annotation_ids)
+# Render annotations on top of the image
+coco.showAnns(annotations)
+plt.imshow(y_train[:, :, n_cats], alpha=0.5); plt.axis('off')
